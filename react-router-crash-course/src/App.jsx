4205cb2699d455f89ee89b6bcfd8ff53c6cb946c
Preston Fassbender <fassbenderp0551@gmail.com>
2024-12-27 14:49:17 -0600

    react router routes, links, navlinks, navigating, 404, nested routes, index routes, url params, search params, relative vs absolute, lazy loading, and auth

--- react-router-crash-course/src/App.jsx
+++ react-router-crash-course/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { Suspense } from 'react'
+import { Route, Routes } from 'react-router'
+import { Home } from './components/Home'
+import { Navbar } from './components/Navbar'
+import { OrderSummary } from './components/OrderSummary'
+import { NoMatch } from './components/NoMatch'
+import { Products } from './components/Products'
+import { FeaturedProducts } from './components/FeaturedProducts'
+import { NewProducts } from './components/NewProducts'
+import { Users } from './components/Users'
+import { UserDetails } from './components/UserDetails'
+import { Admin } from './components/Admin'
+import { Profile } from './components/Profile'
+import { AuthProvider } from './components/auth'
+import { Login } from './components/Login'
+import { RequireAuth } from './components/RequireAuth'
+
+// import { About } from './components/About'
+const LazyAbout = React.lazy(() => import('./components/About'))
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <AuthProvider>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='about' element={<Suspense fallback='Loading...'><LazyAbout /></Suspense>} />
+                    <Route path='order-summary' element={<OrderSummary />} />
+                    <Route path='products' element={<Products />}>
+                        <Route index element={<FeaturedProducts />} />
+                        <Route path='featured' element={<FeaturedProducts />} />
+                        <Route path='new' element={<NewProducts />} />
+                    </Route>
+                    <Route path='users' element={<Users />} >
+                        <Route path=':userId' element={<UserDetails />} />
+                        <Route path='admin' element={<Admin />} />
+                    </Route>
+                    <Route path='profile' element={<RequireAuth><Profile /></RequireAuth>} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='*' element={<NoMatch />} />
+                </Routes>
+            </AuthProvider>
+        </>
+    )
 }
 
 export default App
